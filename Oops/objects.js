95ba5923041807e4097525834cc537873a5c876c
naveenkarthika <yuvannaveen65@gmail.com>
2023-10-05 22:34:49 +0530

    Creating objects

--- Oops/objects.js
+++ Oops/objects.js
@@ -10,4 +10,42 @@ const circle = {
     }
 }
 
-circle.draw()
+circle.draw();
+
+/**
+ * Two way to create objects
+ * 1. factory function - return an object
+ * 2. constructor function - new key word along with the this
+ */
+
+// factory function
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw new');            
+        }
+    }
+}
+
+const circle1 = createCircle(1);
+circle1.draw();
+
+
+// constructor function
+// Note: Constructor function name start with uppercase, we use new key word, it will create the empty object {} and no need to return the values it will automatically return the values.
+
+function Circle(radius) {
+    // console.log('this', this); // Points to Circle object
+    
+    this.radius = radius;
+    this.draw = function() {
+        console.log('const draw');
+    }
+}
+
+const another = new Circle(1); // without using new key word, this will points global window object & return the function values.
+
+another.draw()
+
