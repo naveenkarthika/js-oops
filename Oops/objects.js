34c36933481a4aca79117b61c9956736246e1da1
naveenkarthika <yuvannaveen65@gmail.com>
2023-10-05 23:42:37 +0530

    Value & Refernce type

--- Oops/objects.js
+++ Oops/objects.js
@@ -47,5 +47,74 @@ function Circle(radius) {
 
 const another = new Circle(1); // without using new key word, this will points global window object & return the function values.
 
-another.draw()
+another.draw();
+
+
+// object literals
+let x = {};
+
+// Js internally change the above line into
+// let x = new Object();
+
+// Built-in constructor
+// 1. new String() - '', "", ``
+// 2. new Boolean() - true, false
+// 3. new Number() - 1, 2, 3 ...
+
+
+// Functions are Objects
+
+function Circle1(radius) {    
+    this.radius = radius;
+    this.draw = function() {
+        console.log('const 1 draw');
+    }
+
+    return this;
+}
+
+const anotherWay = Circle1.call({}, 1);
+anotherWay.draw();
+
+
+// Values vs Reference Type
+
+let x1=10;
+let y1=x1;
+x1 = 20;
+
+console.log('x1', x1);
+console.log('y1', y1);
+
+const obj = {
+    value: 10
+}
+const obj1 = obj;
+obj.value = 20;
+
+console.log('obj', obj);
+console.log('obj1', obj1);
+
+// value type
+var number = 10;
+function increase(number) {
+    number++
+}
+increase(number);
+console.log('number', number);
+
+
+// Reference type
+var numberObj = {
+    value: 10
+};
+
+function increaseObj(numberObj) {
+    numberObj.value++
+}
+
+increaseObj(numberObj);
+console.log('numberObj', numberObj);
+
+
 
